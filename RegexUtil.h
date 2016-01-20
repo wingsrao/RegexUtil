b3616b49ae9367e404613dda77526f62899c5f1b
Vince <451406018@qq.com>
2016-01-20 11:08:30 +0800

    the util files
    Download and copy into your Projects.

---
+++ RegexUtil.h
@@ -0,0 +1,47 @@
+//
+//  RegexUtil.h
+//
+//  Created by 451406018@qq.com on 16/1/13.
+//  Copyright © 2016年 wings. All rights reserved.
+//  >>>>>别喷我用英文写注视，外国人根本不看。。。
+//
+
+#import <Foundation/Foundation.h>
+
+typedef enum : NSUInteger {
+    RegexTypeEmail = 1,     //email
+    RegexTypeDigit,         //0~9 number
+    RegexTypeMoney,         //Chinese RMB e.g. ￥20.88（without the money symbol），can't be ￥01.88
+    RegexTypePassword,      //password restrict e.g. password only contains number and character,character must be first
+    RegexTypeID,            //China ID.
+    RegexTypePhone          //China Phone format
+} RegexType;
+
+#define kEmailRegex     @"^\\w+([-+.]\\w+)*@\\w+([-.]\\w+)*.\\w+([-.]\\w+)*$"
+#define kDigit          @"^[0-9]+$"
+#define kMoney          @"^[1-9]{1}[0-9]*([.]{1}[0-9]{1,2})?|[0]{1}([.]{1}[0-9]{1,2})?$" //100.99
+#define kID             @"^(\\d{15})|(\\d{18})|(\\d{17}(\\d|X|x))$" // only check format(15 or 18 numbers),not real
+#define kPassword       @"^[a-zA-Z0-9]\\w{6,20}$"
+#define kPhone          @"^1[3|4|5|7|8]\\d{9}$"
+
+@interface RegexUtil : NSObject
+
+/**
+ *  The method to match the String.
+ *
+ *  @param str  string to match
+ *  @param type which type to match
+ *
+ *  @return YES if match,NO if not match
+ */
++(BOOL)isMatch:(NSString *)str withType:(RegexType)type;
+/**
+ *  check the real Chinese ID Number
+ *
+ *  @param value string to check
+ *
+ *  @return YES if real,NO if not real
+ */
++(BOOL)verifyIDCardNumber:(NSString *)value;
+
+@end
---
+++ RegexUtil.m
@@ -0,0 +1,78 @@
+//
+//  RegexUtil.m
+//
+//  Created by 451406018@qq.com on 16/1/13.
+//  Copyright © 2016年 wings. All rights reserved.
+//
+
+#import "RegexUtil.h"
+
+@implementation RegexUtil
+
++(BOOL)isMatch:(NSString *)str withType:(RegexType)type{
+    NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@",[self regex:type]];
+    return [predicate evaluateWithObject:str];
+}
+
++(NSString *)regex:(RegexType)type{
+    switch (type) {
+        case RegexTypeEmail:
+            return kEmailRegex;
+            break;
+        case RegexTypeDigit:
+            return kDigit;
+            break;
+        case RegexTypeID:
+            return kID;
+            break;
+        case RegexTypeMoney:
+            return kMoney;
+            break;
+        case RegexTypePassword:
+            return kPassword;
+            break;
+        case RegexTypePhone:
+            return kPhone;
+            break;
+        default:
+            return @"";
+            break;
+    }
+}
+
++(BOOL)verifyIDCardNumber:(NSString *)value{
+    value = [value stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]];
+    if ([value length] != 18) {
+        return NO;
+    }
+    NSString *mmdd = @"(((0[13578]|1[02])(0[1-9]|[12][0-9]|3[01]))|((0[469]|11)(0[1-9]|[12][0-9]|30))|(02(0[1-9]|[1][0-9]|2[0-8])))";
+    NSString *leapMmdd = @"0229";
+    NSString *year = @"(19|20)[0-9]{2}";
+    NSString *leapYear = @"(19|20)(0[48]|[2468][048]|[13579][26])";
+    NSString *yearMmdd = [NSString stringWithFormat:@"%@%@", year, mmdd];
+    NSString *leapyearMmdd = [NSString stringWithFormat:@"%@%@", leapYear, leapMmdd];
+    NSString *yyyyMmdd = [NSString stringWithFormat:@"((%@)|(%@)|(%@))", yearMmdd, leapyearMmdd, @"20000229"];
+    NSString *area = @"(1[1-5]|2[1-3]|3[1-7]|4[1-6]|5[0-4]|6[1-5]|82|[7-9]1)[0-9]{4}";
+    NSString *regex = [NSString stringWithFormat:@"%@%@%@", area, yyyyMmdd  , @"[0-9]{3}[0-9Xx]"];
+    
+    NSPredicate *regexTest = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", regex];
+    if (![regexTest evaluateWithObject:value]) {
+        return NO;
+    }
+    int summary = ([value substringWithRange:NSMakeRange(0,1)].intValue + [value substringWithRange:NSMakeRange(10,1)].intValue) *7
+    + ([value substringWithRange:NSMakeRange(1,1)].intValue + [value substringWithRange:NSMakeRange(11,1)].intValue) *9
+    + ([value substringWithRange:NSMakeRange(2,1)].intValue + [value substringWithRange:NSMakeRange(12,1)].intValue) *10
+    + ([value substringWithRange:NSMakeRange(3,1)].intValue + [value substringWithRange:NSMakeRange(13,1)].intValue) *5
+    + ([value substringWithRange:NSMakeRange(4,1)].intValue + [value substringWithRange:NSMakeRange(14,1)].intValue) *8
+    + ([value substringWithRange:NSMakeRange(5,1)].intValue + [value substringWithRange:NSMakeRange(15,1)].intValue) *4
+    + ([value substringWithRange:NSMakeRange(6,1)].intValue + [value substringWithRange:NSMakeRange(16,1)].intValue) *2
+    + [value substringWithRange:NSMakeRange(7,1)].intValue *1 + [value substringWithRange:NSMakeRange(8,1)].intValue *6
+    + [value substringWithRange:NSMakeRange(9,1)].intValue *3;
+    NSInteger remainder = summary % 11;
+    NSString *checkBit = @"";
+    NSString *checkString = @"10X98765432";
+    checkBit = [checkString substringWithRange:NSMakeRange(remainder,1)];// 判断校验位
+    return [checkBit isEqualToString:[[value substringWithRange:NSMakeRange(17,1)] uppercaseString]];
+}
+
+@end
